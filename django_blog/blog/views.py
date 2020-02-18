ed364888ce805e57b6575cbf3d706a4d1cd4a23c
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-18 11:49:51 -0800

    now reading post data from database

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'AlfonsoMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 27, 2020'
-    },
-    {
-        'author': 'AlfonsoMS',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 28, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
